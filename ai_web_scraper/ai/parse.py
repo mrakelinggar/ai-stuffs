a007418c43949744653ea2e32d1ad91abfaa2e6f
Rake Linggar <rake.linggar@gmail.com>
2025-06-30 15:08:02 -0400

    AI Web Scrapper complete

--- ai_web_scraper/ai/parse.py
+++ ai_web_scraper/ai/parse.py
@@ -1,8 +1,7 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
-
-
+# Create the template
 template = (
     "You are tasked with extracting specific information from the following text content: {dom_content}. "
     "Please follow these instructions carefully: \n\n"
@@ -12,7 +11,20 @@ template = (
     "4. **Direct Data Only:** Your output should contain only the data that is explicitly requested, with no other text."
 )
 
-model = OllamaLLM(model='llama3')
+model = OllamaLLM(model='llama3.2')
 
 def parse_with_ollama(dom_chunks, parse_description):
-    pass
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+
+    parsed_result = []
+
+    # for every chunk, parse it
+    for i, chunk in enumerate(dom_chunks, start=1):
+        response = chain.invoke({"dom_content":chunk, "parse_description": parse_description})
+
+        print(f"Parsed batch {i} of {len(dom_chunks)}")
+
+        parsed_result.append(response)
+
+    return "\n".join(parsed_result)
